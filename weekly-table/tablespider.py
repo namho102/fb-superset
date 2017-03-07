2cbbad111be9875f729f910fe454aa935269c2b4
Hoang Nam <namhoang.td09@gmail.com>
2017-03-07 22:11:50 +0000

    update  weekly table

--- weekly-table/tablespider.py
+++ weekly-table/tablespider.py
@@ -1,9 +1,15 @@
 import scrapy
 import time
 
+# urls = ['http://www.worldfootball.net/schedule/eng-premier-league-2016-2017-spieltag/' + str(x + 1) for x in range(20)]
+# print(urls)
+
+URL = "http://www.worldfootball.net/schedule/eng-premier-league-2016-2017-spieltag/%d"
+
 class TableSpider(scrapy.Spider):
     name = 'tablespider'
-    start_urls = ['http://www.worldfootball.net/schedule/eng-premier-league-2016-2017-spieltag/1/', 'http://www.worldfootball.net/schedule/eng-premier-league-2016-2017-spieltag/2/']
+    start_urls = [URL % 1]
+    page_number = 1
 
     def parse(self, response):
 
@@ -11,6 +17,9 @@ class TableSpider(scrapy.Spider):
         	yield {'team': row.css('td:nth-child(3) a ::text').extract_first(), 'pts': row.css('td:nth-child(10) ::text').extract_first()}
 
         # next_page = response.css('div.prev-post > a ::attr(href)').extract_first()
+        self.page_number += 1
+        if self.page_number <= 26:
+        	yield scrapy.Request(URL % self.page_number, callback=self.parse)
         # if next_page:
         #     yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
 
